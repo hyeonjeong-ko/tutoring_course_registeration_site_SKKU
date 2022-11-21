d71db1d517effa942faee65f881b26dfde5468d9
Test <test@example.com>
2022-11-21 17:27:46 +0900

    update DB for lectures

--- everytime_crawling/csvtoserver.py
+++ everytime_crawling/csvtoserver.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
 
 import requests
+import json
 import csv
 
 url = "https://rpyy83l3r1.execute-api.ap-northeast-2.amazonaws.com/dev"
 
-def postcourse(params):
-    response = requests.post(url + "/", params)
+def postLecture(data):
+    response = requests.post(url + "/addlecture", json=(data))
+    return response
 
 
 if __name__ == "__main__":
-    with open("everytime_info_6.csv", newline="", encoding= "UTF8") as csvfile:
+    with open("everytime_info_1.csv", newline="", encoding= "UTF8") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=",", quotechar="\n")
         for row in spamreader:
+            if row[1] == "전공영역2":
+                continue
             temp2 = {
 
             }
             temp2["college"] = row[0]
-            temp2["dep"] = row[1]
-            temp2["course_id"] = row[3]
+            temp2["dept"] = row[1]
+            temp2["id"] = row[3]
             temp2["name"] = row[4]
-            temp2["professor"] = row[5]
+            temp2["prof"] = row[5]
 
-            print(temp2)
+            # print(temp2)
+            res = postLecture(temp2)
+            print(res)
+            
             
