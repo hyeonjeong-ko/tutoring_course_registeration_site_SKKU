0fd894ef508f5723a64364b0fc90b3ad2e388225
Test <test@example.com>
2022-11-21 17:41:58 +0900

    DB for lecture  updated

--- everytime_crawling/csvtoserver.py
+++ everytime_crawling/csvtoserver.py
@@ -10,24 +10,35 @@ def postLecture(data):
     response = requests.post(url + "/addlecture", json=(data))
     return response
 
+def dept(department):
+    if department == "시스템경영공학과":
+        return True
+    elif department == "소프트웨어학과":
+        return True
+    elif department == "물리학과":
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
-    with open("everytime_info_1.csv", newline="", encoding= "UTF8") as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=",", quotechar="\n")
-        for row in spamreader:
-            if row[1] == "전공영역2":
-                continue
-            temp2 = {
+    for i in range(1,7):
+        with open("everytime_info_"+str(i) + ".csv", newline="", encoding= "UTF8") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=",", quotechar="\n")
+            for row in spamreader:
+                if row[1] == "전공영역2":
+                    continue
+                elif dept(row[1]):
+                    temp2 = {
 
-            }
-            temp2["college"] = row[0]
-            temp2["dept"] = row[1]
-            temp2["id"] = row[3]
-            temp2["name"] = row[4]
-            temp2["prof"] = row[5]
+                    }
+                    temp2["college"] = row[0]
+                    temp2["dept"] = row[1]
+                    temp2["id"] = row[3]
+                    temp2["name"] = row[4]
+                    temp2["prof"] = row[5]
 
-            # print(temp2)
-            res = postLecture(temp2)
-            print(res)
+                    # print(temp2)
+                    res = postLecture(temp2)
+                    print(res)
             
             
