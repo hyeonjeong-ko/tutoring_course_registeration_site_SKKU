314f19e96a0283d2eae6f4fb9c504b6862f4ddce
Kohyeonjeong <toyu7870@naver.com>
2022-12-01 02:31:22 +0900

    appli_index_main_style_update

--- application.js
+++ application.js
@@ -98,7 +98,7 @@ response.then( (result) => {
             let new_course = document.createElement("b");
             new_course.textContent = temp.name;
             contents.appendChild(new_course);
-            console.log(temp);
+            //console.log(temp);
             // courses.push(courses_name[element]) -> 요렇게 해서 밖에서 console.log(courses)라고 해도 작동하지 않습니다. undefined가 나와요.
         });
     //해당 항목은 application_mentee.html에 과목 목록을 만드는 데에 필요할 것이라 생각합니다. 
@@ -124,10 +124,12 @@ mycourses.then( (result) => {
             
 
             //요런식으로 추가하시면 될 것 같습니다.
+/*
             let new_course = document.createElement("b");
             new_course.textContent = temp.name;
             curApplication_form.appendChild(new_course);
             console.log(temp);
+  */                      
             // courses.push(courses_name[element]) -> 요렇게 해서 밖에서 console.log(courses)라고 해도 작동하지 않습니다. undefined가 나와요.
         });
     //해당 항목은 application_mentee.html에 과목 목록을 만드는 데에 필요할 것이라 생각합니다. 
@@ -145,6 +147,238 @@ mycourses.then( (result) => {
             temp.name       = result["lectures"][element]["name"];
             temp.professor  = result["lectures"][element]["professor"];
 
-            console.log(temp);
+            //console.log(temp);
+
+            let contents = document.querySelector("#contents_all");
+            
         });
+    });
+
+    //학수번호로 과목 찾아 등록하는기능
+    /*
+    bar_enrollbtn.addEventListener("click",function() {
+        let intext1 = document.getElementById("gwamok").value;
+        let intext2 = document.getElementById("ban").value;
+
+        let table = document.getElementsByTagName("table");
+        
+    })*/
+
+
+    
+
+    // table function
+    var result = '';
+    function createTable(p1,p2,p3,p4,p5){
+        var tbl = '<table class="enroll_table">';
+        var tbl2 = '</table>';
+        var th = '<thead>' + '<tr>' + 
+        '<th>' + '더보기' + '</th>' +
+        '<th>' + '신청' + '</th>' + 
+        '<th>' + '학수번호 - 분반' + '</th>' +
+        '<th>' + '교과목명' + '</th>' +
+        '<th>' + '교강사' + '</th>' +
+        '<th>' + 'Tutor' + '</th>' +
+        '<th>' + '수강인원' + '</th>' +
+        '<th>' + '수업시간/온오프' + '</th>'
+        +'</tr>' + '</thead>';
+
+                result += '<tr>' + '<td>' + '<div class="plus_btn">'+ '+' + '</div>' + '</td>' +
+                '<td>' + '<div class="enroll_btn">' + '신청' + '</div>' + '</td>' +
+                 '<td>' + p1 + '</td>' + 
+                '<td>' + p2 + '</td>' +
+                '<td>' + p3 + '</td>' + 
+                '<td>' + p4 + '</td>' + 
+                '<td>' + p5 + '/5' + '</td>' + 
+                '<td>' + '11:00~12:00/offline' + '</td>' +                 
+                '</tr>'
+
+    return tbl + th + result + tbl2 ;
+    }
+
+    // create the table row data
+function insertdata(day,fout,fin,balance) {
+};
+
+var tablebox = document.getElementById("contents")
+response.then( (result) => {
+    Object.keys(result.courses)
+        .forEach(element => {
+            let temp = {
+
+            };
+            temp.id         = result["courses"][element]["id"];
+            temp.name       = result["courses"][element]["name"];
+            temp.professor  = result["courses"][element]["professor"];
+            temp.tutor      = result["courses"][element]["tutor"];
+            temp.tutee      = result["courses"][element]["tutee"];
+            temp.tuteeNum   = result["courses"][element]["tuteeNum"];
+
+            tablebox.innerHTML = createTable(temp.id,temp.name,temp.professor,temp.tutor,temp.tuteeNum);
+            tmp = tablebox.innerHTML;
+            tabletext = tmp.replace('NaN','');
+            tablebox.innerHTML = tabletext;
+
+    
+      
+            
+            let enroll_btn = document.getElementsByClassName('enroll_btn');
+     
+
+            let usertable = document.getElementsByClassName('usertable');
+
+            for(let i = 0; i<enroll_btn.length;i++){
+                enroll_btn[i].addEventListener('click',function(){
+                    console.log(i) //row index
+                    
+                    //등록버튼이벤트구현중
+                    let enroll_table = document.getElementsByClassName('.enroll_table');
+                    var arr = Array.prototype.slice.call( enroll_table )
+                    console.log(arr.length)
+                    
+                    var rowList = enroll_table.rows;
+                    console.log(rowList);
+                    let classnumber = rowList[i].cells[2].InnerHTML; 
+                    let classname = rowList[i].cells[3].InnerHTML;
+                    let prof = rowList[i].cells[4].InnerHTML;
+                    let tutor = rowList[i].cells[5].InnerHTML;
+                    let tuteenum = rowList[i].cells[6].InnerHTML;
+                    let schedule = rowList[i].cells[7].InnerHTML;
+
+                    str = 
+                    '<tr>' +
+                        '<td>' + classnumber + '</td>' + 
+                       '<td>' + classname + '</td>' +
+                       '<td>' + prof + '</td>' + 
+                       '<td>' + tutor+ '</td>' + 
+                       '<td>' + tuteenum +'/5'+ '</td>' + 
+                       '<td>' + schedule + '</td>' +                 
+                       '</tr>'
+
+
+                    nroll_table.in
+                    
+
+                })
+            }
+
+            
+
+            // course select
+            var course_select = document.getElementById("course_select");
+            var option=document.createElement("option");
+            option.text=temp.name;
+            option.value=temp.id ;
+            course_select.add(option);
+            
+        });
+
+    });
+
+//모든 전공 과목들
+var all_result = '';
+function createTable_all(p1,p2,p3,p4,p5){
+    var tbl = '<table>';
+    var tbl2 = '</table>';
+    var th = '<thead>' + '<tr>' + 
+        '<th>' + '더보기' + '</th>' +
+        '<th>' + '신청' + '</th>' + 
+    '<th>' + '대학구분' + '</th>' +
+    '<th>' + '학과' + '</th>' +
+    '<th>' + '대학번호/분반' + '</th>' +
+    '<th>' + '과목명' + '</th>' +
+    '<th>' + '교수' + '</th>' +
+    +'</tr>' + '</thead>';
+
+           all_result += '<tr>' +
+            '<td>' + '<div class="plus_btn">'+ '+' + '</div>' + '</td>' +
+                '<td>' + '<div class="enroll_btn  view_enrollbtn">' + '신청' + '</div>' + '</td>' +
+             '<td>' + p1 + '</td>' + 
+            '<td>' + p2 + '</td>' +
+            '<td>' + p3 + '</td>' + 
+            '<td>' + p4 + '</td>' + 
+            '<td>' + p5 + '</td>' +              
+            '</tr>'
+
+return tbl + th + all_result + tbl2 ;
+}
+
+//모든 전공 과목 업로드
+allLectures.then((result) => {
+    var tablebox_all = document.getElementById("contents_all")
+    Object.keys(result.lectures)
+        .forEach(element => {
+            let temp = {
+
+            };
+            temp.college    = result["lectures"][element]["college"];
+            temp.department = result["lectures"][element]["department"];
+            temp.lecture_id = result["lectures"][element]["lecture_id"];
+            temp.name       = result["lectures"][element]["name"];
+            temp.professor  = result["lectures"][element]["professor"];
+
+             //table 생성
+             //console.log(temp);
+             tablebox_all.innerHTML = createTable_all(temp.college,temp.department,temp.lecture_id,temp.name,temp.professor);
+             tmp = tablebox_all.innerHTML;
+             tabletext = tmp.replace('NaN','');
+             tablebox_all.innerHTML = tabletext;
+
+        });
+    });
+
+
+//유저 테이블 만들기
+var user_result = '';
+function createUserTable(p1,p2,p3,p4,p5){
+    var tbl = '<table class="usertable">';
+    var tbl2 = '</table>';
+    var th = '<thead>' + '<tr>' + 
+    '<th>' + '학수번호 - 분반' + '</th>' +
+    '<th>' + '교과목명' + '</th>' +
+    '<th>' + '교강사' + '</th>' +
+    '<th>' + 'Tutor' + '</th>' +
+    '<th>' + '수강인원' + '</th>' +
+    '<th>' + '수업시간/온오프' + '</th>'
+    +'</tr>' + '</thead>';
+
+            user_result += '<tr>' +
+             '<td>' + p1 + '</td>' + 
+            '<td>' + p2 + '</td>' +
+            '<td>' + p3 + '</td>' + 
+            '<td>' + p4 + '</td>' + 
+            '<td>' + p5 +'/5'+ '</td>' + 
+            '<td>' + '11:00~12:00/offline' + '</td>' +                 
+            '</tr>'
+
+return tbl + th + user_result + tbl2 ;
+}
+
+    //내 수강과목 조회
+    mycourses.then( (result) => {
+        let curApplication_form = document.querySelector("#curApplication_form");
+    
+    Object.keys(result.courses)
+        .forEach(element => {
+            let temp = {
+
+            };
+            temp.id         = result["courses"][element]["id"];
+            temp.name       = result["courses"][element]["name"];
+            temp.professor  = result["courses"][element]["professor"];
+            temp.tutor      = result["courses"][element]["tutor"];
+            temp.tutee      = result["courses"][element]["tutee"];
+            temp.tuteeNum   = result["courses"][element]["tuteeNum"];
+            
+
+            let my_course = document.createElement("b");
+            my_course.innerHTML = '<div class="under_tab" style="text-align:center">'+
+                '수강 신청 내역' + '</div>' 
+                + createUserTable(temp.id,temp.name,temp.professor,temp.tutor,temp.tuteeNum);
+                curApplication_form.appendChild(my_course);
+
+            
+         
+        });
+
     });
--- main.js
+++ main.js
@@ -2,41 +2,72 @@
 
 // check id, pwd
 
+const loginBtn = document.getElementById("loginBtn");
+
+
+//로그인 버튼을 클릭했을때
+loginBtn.addEventListener('click', function(event){
+    let id = document.getElementById('idval').value;
+    let pwd = document.getElementById('pwd').value;
+
+    console.log(logincheck(id,pwd))
+
+    if(id.length==0 || pwd.length==0){
+        alert("Please Enter your ID and Password.");
+    }else{
+        //enterUrl(id,pwd);
+    }
+
+
+});
+
 
 // login button
-function enterUrl() {
+function enterUrl(id,pwd) {
     // id, pw correct/wrong check
-    const id = "2020315791"; //Correct example
-    const name = "Ko"; //Correct examp
-    location.href = 'application.html'
+    //const id = "2020315791"; //Correct example
+    //const name = "Ko"; //Correct examp
+
+    //location.href = 'application.html'
     // const id = "2020312141"; //Wrong example
     // const name = "Jun"; //Wrong example 
-    const check = logincheck(id, name); //아래 logincheck 함수를 확인하세요. return 형식은 Promise입니다.
+    
+  
+
+    const check = logincheck(id, pwd); //아래 logincheck 함수를 확인하세요. return 형식은 Promise입니다.
     //Promise를 parsing하는 대신 아래처럼 사용하세요. allcourse 함수 말고는 return값이 data.success가 true/false로만 나올 것입니다.(당연히 그래야하고요)
+
+    console.log(check);
+
     check.then(
         (data) => {
             if (data.success) {
+                alert("successs");
+
                 location.href = 'application.html' //로그인 성공
 
             }
             else {
-                alert('Wrong id or pwd') //로그인 실패
+                alert('Wrong ID password.'); //로그인 실패
             }
         }
     )
 }
 
 // login with enter button
+/*
 document.onkeyup = function (command) {
     let key = command.keyCode
     if (key === 13) {
         enterUrl()
     }
 }
+*/
 
 //임시주석
-const loginBtn = document.getElementById("loginBtn")
-loginBtn.addEventListener('click', enterUrl)
+//const loginBtn = document.getElementById("loginBtn")
+
+//loginBtn.addEventListener('click', enterUrl)
 
 
 // tutoring lecture application
@@ -58,6 +89,8 @@ async function logincheck(id, name) {
         "id": id,
         "name": name,
     }
+
+    
 
     return await fetch(serverURL + "/login", {
         method: "POST",
