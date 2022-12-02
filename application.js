be14d55e3387f8162729c42d93b93047f6773536
Kohyeonjeong <toyu7870@naver.com>
2022-12-02 18:45:37 +0900

    modal_DB_update

--- application.js
+++ application.js
@@ -265,11 +265,102 @@ response.then( (result) => {
                 // console.log(tutor_score);
                 // console.log(tutor_msg);
 
-                //수정해주세요 죄송합니다 ;ㅅ;
-                location.href = "modal.html?"+course_id
-                // modal("my_modal", course_id);
+                //=============================================================
+                const courseURL =
+                "https://rpyy83l3r1.execute-api.ap-northeast-2.amazonaws.com/dev";
+            let temp = location.href.split("?");
+        
+
+                function modal(id,course_id) {
+                    var zIndex = 9999;
+                    var modal = document.getElementById(id);
+                
+                    const infos = getDetailInfo(course_id);
+                    // console.log(infos);
+                    infos.then(
+                        (result) => {
+                            let syllabus = document.getElementById("tutor syllabus");
+                            let message = document.getElementById("tutor message");
+                            let tutor = document.getElementById("tutor");
+
+                            console.log(syllabus);
+                            console.log(message);
+
+                            syllabus.textContent = result.syllabus;
+                            message.textContent = result.motivation;
+                            tutor.textContent = result.tutor;
+                
+                            
+                        });
+                
+                    // 모달 div 뒤에 희끄무레한 레이어
+                    var bg = document.createElement("div");
+                    bg.setStyle({
+                        position: "fixed",
+                        zIndex: zIndex,
+                        left: "0px",
+                        top: "0px",
+                        width: "100%",
+                        height: "100%",
+                        overflow: "auto",
+                        // 레이어 색갈은 여기서 바꾸면 됨
+                        backgroundColor: "rgba(0,0,0,0.4)",
+                    });
+                    document.body.append(bg);
+                
+                    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
+                    modal
+                        .querySelector(".modal_close_btn")
+                        .addEventListener("click", function () {
+                            bg.remove();
+                            modal.style.display = "none";
+                        });
+                
+                    modal.setStyle({
+                        position: "fixed",
+                        display: "block",
+                        boxShadow:
+                            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                
+                        // 시꺼먼 레이어 보다 한칸 위에 보이기
+                        zIndex: zIndex + 1,
+                
+                        // div center 정렬
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        msTransform: "translate(-50%, -50%)",
+                        webkitTransform: "translate(-50%, -50%)",
+                    });
+                }
+                
+                // Element 에 style 한번에 오브젝트로 설정하는 함수 추가
+                Element.prototype.setStyle = function (styles) {
+                    for (var k in styles) this.style[k] = styles[k];
+                    return this;
+                };
+                
+                async function getDetailInfo(id) {
+                    let data = {
+                        "id": id
+                    }
+                    return await fetch(courseURL + "/getcourse", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(data)
+                    })
+                        .then((response) => response.json());
+                }
+
+
+                //=============================================================
+                modal("my_modal", "SWE2015-41");
                 
             })
+
+            
 
                 
             }
@@ -390,6 +481,12 @@ function insertdata(p1,p2,p3,p4,p5) {
 
     return user_row ;
 };
+
+
+
+
+
+
 
 
 
